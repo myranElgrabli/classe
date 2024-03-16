03c0fe9772d4762f97d8b6e27c14a6340f012831
myranElgrabli <144587957+myranElgrabli@users.noreply.github.com>
2024-03-16 12:57:14 +0200

    Create shiur 10.java

---
+++ shiur 10.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;  
+public class Shiur10.java
+
+public static void f1(int[] arr, int index)
+{
+	if ((index>0)&&(index<arr.length))
+	{
+		if (arr[index]%2!=0)
+		{
+			arr[index]=arr[index]+1;
+		}
+		
+	}
+	else
+        System.out.println("Error!"); 
+}
+
+
+
+{
+	 public static void main(String[] args)
+	 int [] arr= {1,2,3,4,5};
+}
